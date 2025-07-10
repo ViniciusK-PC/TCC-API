b20cdb451cbcd064427adaed93d89c3c39b5c215
Torman <tormantec@outlook.com>
2025-07-09 23:47:33 -0300

    inicial

---
+++ UserManagement/Migrations/20250710011301_NomeDaMigration.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class NomeDaMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Cep",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Cidade",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Documento",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Numero",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Rua",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TipoDocumento",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Uf",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Cep",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Cidade",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Documento",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Numero",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Rua",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "TipoDocumento",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Uf",
+                table: "Users");
+        }
+    }
+}
